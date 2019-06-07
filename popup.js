5c6a99ef0cf1990f21bcc2c8282f85e9642bfae3
David Torres <detour27@gmail.com>
2019-06-07 17:46:49 -0400

    cleanup, handle edge cases, add dok button

--- popup.js
+++ popup.js
@@ -1,5 +1,17 @@
-let libraryAccessBtn = document.getElementById('library-access')
-let libraryText = document.getElementById('library-count')
+let libraryAccessBtn = document.getElementById('access-master-vault')
+let libraryOnlyFavorites = document.getElementById('only-favorites')
+let syncDokBtn = document.getElementById('sync-dok')
+let libraryText = document.getElementById('library-access')
+let dokText = document.getElementById('dok-sync')
+
+loadLibrary().then(function (library) {
+  console.log('loaded library', library)
+  if (!library || Object.keys(library).length == 0) {
+    libraryText.innerHTML = 'No decks accessed from Master Vault'
+  } else {
+    libraryText.innerHTML = Object.keys(library).length + ' decks accessed from Master Vault'
+  }
+})
 
 libraryAccessBtn.onclick = function (el) {
   chrome.cookies.get(
@@ -8,20 +20,31 @@ libraryAccessBtn.onclick = function (el) {
   )
 }
 
+syncDokBtn.onclick = function (el) {
+  loadLibrary().then(function (library) {
+    console.log('loaded library', library)
+    if (!library || Object.keys(library).length == 0) {
+      alert('No decks accessed from Master Vault. Click "Access Master Vault" first.')
+    } else {
+      alert('Implement this')
+    }
+  })
+}
+
 function handleToken (cookie) {
   let token = cookie.value
+  let onlyFavorites = libraryOnlyFavorites.checked ? 1 : 0
   getUser(token).then(function (user) {
-    getLibrary(token, user, 1, 0, []).then(function (library) {
+    getLibrary(token, user, 1, onlyFavorites, []).then(function (library) {
       console.log(library)
       let libraryMin = {}
       library.forEach(deck => {
         libraryMin[deck.id] = deck.name
       })
-      console.log(libraryMin)
 
       chrome.storage.sync.set({ library: libraryMin }, function () {
         console.log('Library saved')
-        libraryText.innerHTML = library.length + ' decks accessed'
+        libraryText.innerHTML = library.length + ' decks accessed from Master Vault'
       })
     })
   })
@@ -45,7 +68,15 @@ function getUser (token) {
     })
 }
 
-function getLibrary (token, user, page, only_favorites, library) {
+function loadLibrary () {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(['library'], function (result) {
+      resolve(result.library)
+    })
+  })
+}
+
+function getLibrary (token, user, page, onlyFavorites, library) {
   return new Promise((resolve, reject) => {
     fetch(
       'https://www.keyforgegame.com/api/users/' +
@@ -53,7 +84,7 @@ function getLibrary (token, user, page, only_favorites, library) {
         '/decks/?page=' +
         page +
         '&page_size=10&search=&power_level=0,11&chains=0,24&only_favorites=' +
-        only_favorites +
+        onlyFavorites +
         '&ordering=-date',
       {
         credentials: 'include',
@@ -74,7 +105,7 @@ function getLibrary (token, user, page, only_favorites, library) {
 
         if (library.length != response.count) {
           page = page + 1
-          getLibrary(token, user, page, only_favorites, library)
+          getLibrary(token, user, page, onlyFavorites, library)
             .then(resolve)
             .catch(reject)
         } else {
